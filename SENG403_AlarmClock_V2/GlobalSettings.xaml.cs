ecc5558d5a708553719d5f19f5270eaf861a1ebd
rajat <rajat.singh@ucalgary.ca>
2017-03-11 14:28:58 -0700

    first commit

---
+++ SENG403_AlarmClock_V2/GlobalSettings.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SENG403_AlarmClock_V2
+{
+    /// <summary>
+    /// Interaction logic for GlobalSettings.xaml
+    /// </summary>
+    public partial class GlobalSettings : Window
+    {
+        public GlobalSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Global_Mice(object sender, MouseButtonEventArgs e)
+        {
+
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
---
+++ SENG403_AlarmClock_V2/NewAlarmWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SENG403_AlarmClock_V2
+{
+    /// <summary>
+    /// Interaction logic for NewAlarmWindow.xaml
+    /// </summary>
+    public partial class NewAlarmWindow : Window
+    {
+        public NewAlarmWindow()
+        {
+            InitializeComponent();
+            RadioGrid.Visibility = Visibility.Collapsed;
+            OtherProps.Margin = new Thickness(26, 149, 21, 34);
+
+        }
+
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radioButton_Thu_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void repeat_checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioGrid.Visibility = Visibility.Visible;
+            OtherProps.Margin = new Thickness(26, 230, 21, 34);
+        }
+
+        private void Checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            RadioGrid.Visibility = Visibility.Collapsed;
+            OtherProps.Margin = new Thickness(26, 149, 21, 34);
+        }
+    }
+}
